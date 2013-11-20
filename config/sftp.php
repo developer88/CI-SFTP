fb415ecfaeb18d67016b550b2263c6a19561b769
Andrey Eremin <dsoft88@gmail.com>
2013-11-20 11:18:13 +0400

    Initial release.

---
+++ config/sftp.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['hostname']	   = '172.16.100.154';
+$config['username']    = 'lc';
+$config['password']	   = 'ATCq[e13!@#$67';
+$config['default_dir'] = '/LC/';
+$config['autoconnect'] = true;
